57afa40252afe2df0a1b9bff9d28ea18250b67c7
VinRis <71922931+VinRis@users.noreply.github.com>
2025-12-28 00:55:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
+const CACHE_NAME = 'farm-manager-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/js/utils.js',
+  '/js/db.js',
+  '/js/auth.js',
+  '/js/sync.js',
+  '/js/ui.js',
+  '/js/forms.js',
+  '/js/charts.js',
+  '/js/pdf.js',
+  '/manifest.json',
+  '/icons/icon-72x72.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+});
